115cf8231aab7fb72c4c5be4d0fdd2228462d68e
TAR UMT <TAR UMT@PC98>
2024-03-07 15:55:00 +0800

    Good Morning

---
+++ src/simpleapp/BigDuck.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package simpleapp;
+
+/**
+ *
+ * @author TAR UMT
+ */
+public class BigDuck {
+    public static void main(String[] args) {
+        System.out.println("I Am Big");
+
+    }
+}
--- src/simpleapp/SimpleApp.java
+++ src/simpleapp/SimpleApp.java
@@ -14,6 +14,7 @@ public class SimpleApp {
     public static void main(String[] args) {
         System.out.println("Happy Birthday");
         System.out.println("Good Morning");
+        System.out.println("Hello");
     }
     
 }

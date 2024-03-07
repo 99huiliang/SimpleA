35b9597abcc0ec35753d3f30bc3faf0f2b2f4ab2
TAR UMT <TAR UMT@PC98>
2024-03-07 15:36:56 +0800

    Good Morning

--- src/simpleapp/SimpleApp.java
+++ src/simpleapp/SimpleApp.java
@@ -13,6 +13,7 @@ public class SimpleApp {
     
     public static void main(String[] args) {
         System.out.println("Happy Birthday");
+        System.out.println("Good Morning");
     }
     
 }
